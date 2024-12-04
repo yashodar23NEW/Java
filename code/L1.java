9be2b84077f1a5d14134f3dc83d1ef38c14e6509
Ruwanthi Yashodara <119606787+yashodar23NEW@users.noreply.github.com>
2024-12-04 23:41:10 +0530

    Create L1.java

---
+++ code/L1.java
@@ -0,0 +1,11 @@
+
+public class Main {
+	public static void main(String[] aregs)
+	{
+		System.out.println("\tI am coarding.\n");
+		System.out.println("\"I am coarding.\"");//escape sequence
+		
+	}
+
+	
+}
