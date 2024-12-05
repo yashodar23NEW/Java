248bc56adff9f5958e7507ce24adcdd539cf6905
Ruwanthi Yashodara <yashodararuwanthi@gmail.com>
2024-12-05 22:17:26 +0530

    complete variables lesson

---
+++ L2/src/L2/main.java
@@ -0,0 +1,29 @@
+package L2;
+
+public class main {
+
+	public static void main(String[] args) {
+		int x=129;
+		double y=45.12;
+		boolean z=true;
+		char simble='@';
+		String name="Nimal";
+		
+		System.out.println("Hello "+name);
+		
+		String a="water";
+		String b="juice";
+		String temp;
+		
+		
+		temp="water";
+		a="juice";
+		b="water";
+		
+		System.out.println("a is: "+a);
+		System.out.println("b is : "+b);
+	
+
+	}
+
+}
