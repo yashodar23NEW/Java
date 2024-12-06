da72e02ce160f63ba0c1a3d702ecc7a99e25b9d8
Ruwanthi Yashodara <yashodararuwanthi@gmail.com>
2024-12-06 21:55:02 +0530

    ArithmaticOperators

---
+++ L4/src/L4/main.java
@@ -0,0 +1,17 @@
+package L4;
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		int students=10;
+		//students+=1;
+		//students-=1;
+		//students =students *3;
+		students=students/3;
+		System.out.println("number of students: "+students);
+		
+
+	}
+
+}
